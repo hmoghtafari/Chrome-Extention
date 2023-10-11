93b610d09cab18b9ca2bb070a1ae43fc3baed913
Hossein Moghtaferi <h.moghtafari@hotmail.com>
2023-10-11 08:47:18 +0400

    adding crypto

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const author = document.getElementById("author");
+const cryptoTop = document.getElementById("crypto-top");
+const crypto = document.getElementById("crypto");
 function imgbackground() {
   fetch(
     "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
@@ -10,7 +12,41 @@ function imgbackground() {
     })
     .catch((error) => {
       console.error(error);
-      document.body.style.backgroundColor = `green`;
+      document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1694675856558-bc858408e60c?ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTY5MDk2ODN8&ixlib=rb-4.0.3)`;
+      author.textContent = `By: Hossein Moghtafari`;
     });
 }
 imgbackground();
+
+function getCrypto() {
+  fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then((res) => {
+      if (!res.ok) {
+        console.log(res.status);
+        throw Error ("Something went wrong!")
+      }
+     return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      cryptoTop.innerHTML = `
+      <img src = ${data.image.small} />
+      <span>${data.name}</span>
+      `;
+
+        crypto.innerHTML += `
+      <p>🎯: $${data.market_data.current_price.usd}</p>
+      <p>👆: $${data.market_data.high_24h.usd}</p>
+      <p>👇: $${data.market_data.low_24h.usd}</p>
+      `;
+    
+
+      // data.name,
+      //   data.image.small,
+      //   data.market_data.current_price.usd,
+      //   data.market_data.high_24h.usd,
+      //   data.market_data.low_24h.usd;
+    })
+    .catch((err) => console.error(Error));
+}
+getCrypto();
